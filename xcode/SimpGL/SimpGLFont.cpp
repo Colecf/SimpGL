79fea1356bc93d8dabf90b64170b3a948f0706b5
Cole Faust <colecfaust@gmail.com>
2013-03-03 18:13:28 -0800

    Bug Fixes
    Fixed uninitialized colors on sprites
    Fixed textures not freeing
    Updates xcode settings
    Began work on text rendering

---
+++ xcode/SimpGL/SimpGLFont.cpp
@@ -0,0 +1,9 @@
+//
+//  SimpGLFont.cpp
+//  SimpGL
+//
+//  Created by Cole Faust on 2/9/13.
+//  Copyright (c) 2013 Cole Faust. All rights reserved.
+//
+
+#include "SimpGLFont.h"
---
+++ xcode/SimpGL/SimpGLFont.h
@@ -0,0 +1,14 @@
+//
+//  SimpGLFont.h
+//  SimpGL
+//
+//  Created by Cole Faust on 2/9/13.
+//  Copyright (c) 2013 Cole Faust. All rights reserved.
+//
+
+#ifndef __SimpGL__SimpGLFont__
+#define __SimpGL__SimpGLFont__
+
+#include <iostream>
+
+#endif /* defined(__SimpGL__SimpGLFont__) */
